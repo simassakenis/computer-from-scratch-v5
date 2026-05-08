b26af20dca769eabc7882d836c0accf59eea1884
Simas Šakenis <s.sakenis@gmail.com>
2026-05-08 18:00:36 +0200

    Add OS program

--- computer.py
+++ computer.py
@@ -4,6 +4,28 @@ import time
 
 console_address = 1000072
 
+MOVE = 1
+MOVE_NUMBER = 2
+MOVE_FROM_POINTER = 3
+MOVE_TO_POINTER = 4
+ADD = 5
+ADD_NUMBER = 6
+SUBTRACT = 7
+SHIFT_LEFT = 8
+SHIFT_LEFT_BY_NUMBER = 9
+SHIFT_RIGHT = 10
+SHIFT_RIGHT_BY_NUMBER = 11
+BITWISE_AND = 12
+BITWISE_AND_WITH_NUMBER = 13
+PUSH_NUMBER = 14
+COMPARE = 15
+COMPARE_TO_NUMBER = 16
+JUMP_IF_EQUAL = 17
+JUMP_IF_GREATER = 18
+JUMP = 19
+CALL = 20
+RETURN = 21
+
 def as8(value):
     # Store one machine value as 8 big-endian bytes
     value = value % (2**64)
@@ -29,6 +51,608 @@ def asint(value):
     return result
 
 
+def assemble(source, start_address):
+    labels = {}
+    address = start_address
+    i = 0
+
+    while i < len(source):
+        if isinstance(source[i], str):
+            labels[source[i]] = address
+            i += 1
+        else:
+            address += 24
+            i += 3
+
+    program = []
+    i = 0
+
+    while i < len(source):
+        if isinstance(source[i], str):
+            i += 1
+            continue
+
+        opcode = source[i]
+        operand1 = source[i + 1]
+        operand2 = source[i + 2]
+
+        if isinstance(operand1, str):
+            operand1 = labels[operand1]
+        if isinstance(operand2, str):
+            operand2 = labels[operand2]
+
+        program += as8(opcode)
+        program += as8(operand1)
+        program += as8(operand2)
+        i += 3
+
+    return program
+
+
+operating_system_source = [
+    "terminal",
+    PUSH_NUMBER, 1000064, 0,
+    PUSH_NUMBER, 1000072, 0,
+    MOVE_TO_POINTER, 1, 0,
+    MOVE_NUMBER, 1000000, 0,
+    MOVE_NUMBER, 2000000, 1,
+    MOVE_TO_POINTER, 1, 0,
+    MOVE_FROM_POINTER, 0, 1,
+    PUSH_NUMBER, 0, 0,
+    JUMP, "terminalLoop", 0,
+
+    "terminalLoop",
+    CALL, "listenForKeypress", 0,
+    COMPARE_TO_NUMBER, 2, 8,
+    JUMP_IF_EQUAL, "ifBackspace", 0,
+    JUMP, "elseBackspace", 0,
+
+    "ifBackspace",
+    PUSH_NUMBER, 0, 0,
+    MOVE_FROM_POINTER, 0, 3,
+    COMPARE, 3, 1,
+    JUMP_IF_GREATER, "ifNonEmptyInput", 0,
+    JUMP, "terminalLoop", 0,
+
+    "ifNonEmptyInput",
+    CALL, "removeLastCharacterFromTranscript", 0,
+    JUMP, "terminalLoop", 0,
+
+    "elseBackspace",
+    CALL, "writeToTranscript", 0,
+    COMPARE_TO_NUMBER, 2, 10,
+    JUMP_IF_EQUAL, "ifEnter", 0,
+    JUMP, "terminalLoop", 0,
+
+    "ifEnter",
+    PUSH_NUMBER, 0, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 1, 4,
+    CALL, "parse8ByteValue", 0,
+    MOVE_NUMBER, 0, 4,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 1, 5,
+    ADD_NUMBER, 128, 5,
+    CALL, "parse8ByteValue", 0,
+    ADD_NUMBER, 128, 5,
+    PUSH_NUMBER, 0, 0,
+    MOVE_FROM_POINTER, 0, 6,
+    SUBTRACT, 5, 6,
+    ADD_NUMBER, -8, 6,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 3, 7,
+    PUSH_NUMBER, 3000000, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 4, 9,
+    CALL, "readFromDisk", 0,
+    MOVE_NUMBER, 16, 7,
+    MOVE_FROM_POINTER, 7, 8,
+    MOVE_NUMBER, 3000000, 9,
+    ADD, 4, 9,
+    MOVE_TO_POINTER, 5, 9,
+    ADD_NUMBER, 8, 9,
+    MOVE_TO_POINTER, 6, 9,
+    ADD_NUMBER, 8, 9,
+    MOVE_TO_POINTER, 9, 7,
+    CALL, 3000000, 0,
+    MOVE_TO_POINTER, 8, 7,
+    MOVE_FROM_POINTER, 0, 1,
+    JUMP, "terminalLoop", 0,
+
+    "listenForKeypress",
+    PUSH_NUMBER, 1000048, 0,
+    PUSH_NUMBER, 1, 0,
+    MOVE_TO_POINTER, 1, 0,
+    JUMP, "listenForKeypressLoop", 0,
+
+    "listenForKeypressLoop",
+    MOVE_FROM_POINTER, 0, 1,
+    COMPARE_TO_NUMBER, 1, 0,
+    JUMP_IF_EQUAL, "listenForKeypressLoopExit", 0,
+    JUMP, "listenForKeypressLoop", 0,
+
+    "listenForKeypressLoopExit",
+    MOVE_NUMBER, 1000056, 1,
+    MOVE_FROM_POINTER, 1, -3,
+    RETURN, 0, 0,
+
+    "removeLastCharacterFromTranscript",
+    PUSH_NUMBER, 1000000, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE_FROM_POINTER, 0, 1,
+    COMPARE_TO_NUMBER, 1, 2000000,
+    JUMP_IF_GREATER, "regularRemoveLastCharacter", 0,
+    JUMP, "removeLastCharacterExit", 0,
+
+    "regularRemoveLastCharacter",
+    PUSH_NUMBER, 0, 0,
+    ADD_NUMBER, -8, 1,
+    MOVE_TO_POINTER, 2, 1,
+    MOVE_TO_POINTER, 1, 0,
+    JUMP, "removeLastCharacterFromConsole", 0,
+
+    "removeLastCharacterFromConsole",
+    MOVE_NUMBER, 1000064, 0,
+    MOVE_FROM_POINTER, 0, 1,
+    COMPARE_TO_NUMBER, 1, 1000072,
+    JUMP_IF_GREATER, "regularRemoveLastCharacterFromConsole", 0,
+    JUMP, "removeLastCharacterExit", 0,
+
+    "regularRemoveLastCharacterFromConsole",
+    ADD_NUMBER, -8, 1,
+    MOVE_TO_POINTER, 2, 1,
+    MOVE_TO_POINTER, 1, 0,
+    JUMP, "removeLastCharacterExit", 0,
+
+    "removeLastCharacterExit",
+    RETURN, 0, 0,
+
+    "writeToTranscript",
+    PUSH_NUMBER, 1000000, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE_FROM_POINTER, 0, 1,
+    MOVE_TO_POINTER, -3, 1,
+    ADD_NUMBER, 8, 1,
+    MOVE_TO_POINTER, 1, 0,
+    JUMP, "writeToConsole", 0,
+
+    "writeToConsole",
+    MOVE_NUMBER, 1000064, 0,
+    MOVE_FROM_POINTER, 0, 1,
+    COMPARE_TO_NUMBER, 1, 1032840,
+    JUMP_IF_EQUAL, "shiftConsoleBack", 0,
+    JUMP, "regularWriteToConsole", 0,
+
+    "shiftConsoleBack",
+    ADD_NUMBER, -8, 1,
+    PUSH_NUMBER, 1000080, 0,
+    PUSH_NUMBER, 1000072, 0,
+    PUSH_NUMBER, 0, 0,
+    JUMP, "shiftConsoleBackLoop", 0,
+
+    "shiftConsoleBackLoop",
+    MOVE_FROM_POINTER, 2, 4,
+    MOVE_TO_POINTER, 4, 3,
+    ADD_NUMBER, 8, 2,
+    ADD_NUMBER, 8, 3,
+    COMPARE_TO_NUMBER, 2, 1032840,
+    JUMP_IF_EQUAL, "regularWriteToConsole", 0,
+    JUMP, "shiftConsoleBackLoop", 0,
+
+    "regularWriteToConsole",
+    MOVE_TO_POINTER, -3, 1,
+    ADD_NUMBER, 8, 1,
+    MOVE_TO_POINTER, 1, 0,
+    RETURN, 0, 0,
+
+    "readFromDisk",
+    PUSH_NUMBER, 1000008, 0,
+    MOVE_TO_POINTER, -5, 0,
+    MOVE_NUMBER, 1000016, 0,
+    MOVE_TO_POINTER, -4, 0,
+    MOVE_NUMBER, 1000024, 0,
+    MOVE_TO_POINTER, -3, 0,
+    MOVE_NUMBER, 1000032, 0,
+    PUSH_NUMBER, 1, 0,
+    MOVE_TO_POINTER, 1, 0,
+    JUMP, "readFromDiskLoop", 0,
+
+    "readFromDiskLoop",
+    MOVE_FROM_POINTER, 0, 1,
+    COMPARE_TO_NUMBER, 1, 0,
+    JUMP_IF_EQUAL, "readFromDiskLoopExit", 0,
+    JUMP, "readFromDiskLoop", 0,
+
+    "readFromDiskLoopExit",
+    RETURN, 0, 0,
+
+    "writeToDisk",
+    PUSH_NUMBER, 1000008, 0,
+    MOVE_TO_POINTER, -5, 0,
+    MOVE_NUMBER, 1000016, 0,
+    MOVE_TO_POINTER, -4, 0,
+    MOVE_NUMBER, 1000024, 0,
+    MOVE_TO_POINTER, -3, 0,
+    MOVE_NUMBER, 1000040, 0,
+    PUSH_NUMBER, 1, 0,
+    MOVE_TO_POINTER, 1, 0,
+    JUMP, "writeToDiskLoop", 0,
+
+    "writeToDiskLoop",
+    MOVE_FROM_POINTER, 0, 1,
+    COMPARE_TO_NUMBER, 1, 0,
+    JUMP_IF_EQUAL, "writeToDiskLoopExit", 0,
+    JUMP, "writeToDiskLoop", 0,
+
+    "writeToDiskLoopExit",
+    RETURN, 0, 0,
+
+    "readFromDiskProgram",
+    COMPARE_TO_NUMBER, -3, 256,
+    JUMP_IF_EQUAL, "readFromDiskProgramValidInput", 0,
+    JUMP, "readFromDiskProgramInvalidInput", 0,
+
+    "readFromDiskProgramInvalidInput",
+    RETURN, 0, 0,
+
+    "readFromDiskProgramValidInput",
+    PUSH_NUMBER, 0, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE, -4, 1,
+    CALL, "parse8ByteValue", 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE, -4, 2,
+    ADD_NUMBER, 128, 2,
+    CALL, "parse8ByteValue", 0,
+    MOVE_NUMBER, 8, 2,
+    PUSH_NUMBER, 0, 0,
+    MOVE_FROM_POINTER, 2, 3,
+    ADD_NUMBER, 56, 3,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 3, 4,
+    ADD, 1, 4,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 4, 5,
+    MOVE_TO_POINTER, 0, 5,
+    ADD_NUMBER, 8, 5,
+    MOVE_TO_POINTER, 3, 5,
+    ADD_NUMBER, 8, 5,
+    MOVE_TO_POINTER, 1, 5,
+    ADD_NUMBER, 8, 5,
+    PUSH_NUMBER, 16, 0,
+    MOVE_TO_POINTER, 5, 6,
+    CALL, "readFromDisk", 0,
+    ADD_NUMBER, -8, 5,
+    MOVE, 3, 0,
+    MOVE, 4, 1,
+    ADD_NUMBER, -8, 1,
+    JUMP, "readFromDiskProgramPrintLoop", 0,
+
+    "readFromDiskProgramPrintLoop",
+    COMPARE, 0, 1,
+    JUMP_IF_GREATER, "readFromDiskProgramPrintLoopExit", 0,
+    JUMP, "readFromDiskProgramPrintLoopBody", 0,
+
+    "readFromDiskProgramPrintLoopBody",
+    MOVE_FROM_POINTER, 0, 2,
+    MOVE_TO_POINTER, 2, 5,
+    CALL, "print8ByteValue", 0,
+    ADD_NUMBER, 8, 0,
+    JUMP, "readFromDiskProgramPrintLoop", 0,
+
+    "readFromDiskProgramPrintLoopExit",
+    RETURN, 0, 0,
+
+    "writeToDiskProgram",
+    COMPARE_TO_NUMBER, -3, 128,
+    JUMP_IF_GREATER, "writeToDiskProgramValidInput", 0,
+    JUMP, "writeToDiskProgramInvalidInput", 0,
+
+    "writeToDiskProgramInvalidInput",
+    RETURN, 0, 0,
+
+    "writeToDiskProgramValidInput",
+    PUSH_NUMBER, 0, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE, -4, 1,
+    CALL, "parse8ByteValue", 0,
+    ADD_NUMBER, 128, 1,
+    PUSH_NUMBER, 8, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE_FROM_POINTER, 2, 3,
+    ADD_NUMBER, 56, 3,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 3, 4,
+    ADD_NUMBER, 8, 4,
+    PUSH_NUMBER, 0, 0,
+    MOVE, 3, 5,
+    ADD_NUMBER, 16, 5,
+    PUSH_NUMBER, 0, 0,
+    MOVE, -4, 6,
+    ADD, -3, 6,
+    ADD_NUMBER, -16, 6,
+    MOVE_NUMBER, 16, 2,
+    JUMP, "writeToDiskProgramReadLoop", 0,
+
+    "writeToDiskProgramReadLoop",
+    COMPARE, 1, 6,
+    JUMP_IF_GREATER, "writeToDiskProgramReadLoopExit", 0,
+    JUMP, "writeToDiskProgramReadLoopBody", 0,
+
+    "writeToDiskProgramReadLoopBody",
+    MOVE_TO_POINTER, 1, 4,
+    MOVE_TO_POINTER, 5, 1,
+    CALL, "parse8ByteValue", 0,
+    ADD_NUMBER, 16, 1,
+    ADD_NUMBER, 8, 4,
+    ADD_NUMBER, 8, 5,
+    JUMP, "writeToDiskProgramReadLoop", 0,
+
+    "writeToDiskProgramReadLoopExit",
+    ADD_NUMBER, -8, 4,
+    MOVE, 4, 5,
+    SUBTRACT, 3, 5,
+    MOVE_TO_POINTER, 0, 4,
+    ADD_NUMBER, 8, 4,
+    MOVE_TO_POINTER, 3, 4,
+    ADD_NUMBER, 8, 4,
+    MOVE_TO_POINTER, 5, 4,
+    ADD_NUMBER, 8, 4,
+    MOVE_TO_POINTER, 4, 2,
+    CALL, "writeToDisk", 0,
+    RETURN, 0, 0,
+
+    "parse8ByteValue",
+    PUSH_NUMBER, 0, 0,
+    MOVE, -3, 0,
+    PUSH_NUMBER, 0, 0,
+    MOVE, -3, 1,
+    ADD_NUMBER, 120, 1,
+    PUSH_NUMBER, 0, 0,
+    PUSH_NUMBER, 0, 0,
+    PUSH_NUMBER, 0, 0,
+    JUMP, "parse8ByteValueLoop", 0,
+
+    "parse8ByteValueLoop",
+    COMPARE, 0, 1,
+    JUMP_IF_GREATER, "parse8ByteValueLoopExit", 0,
+    JUMP, "parse8ByteValueLoopBody", 0,
+
+    "parse8ByteValueLoopBody",
+    MOVE_FROM_POINTER, 0, 3,
+    MOVE_NUMBER, 0, 4,
+    COMPARE_TO_NUMBER, 3, 48,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody0", 0,
+    COMPARE_TO_NUMBER, 3, 49,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody1", 0,
+    COMPARE_TO_NUMBER, 3, 50,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody2", 0,
+    COMPARE_TO_NUMBER, 3, 51,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody3", 0,
+    COMPARE_TO_NUMBER, 3, 52,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody4", 0,
+    COMPARE_TO_NUMBER, 3, 53,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody5", 0,
+    COMPARE_TO_NUMBER, 3, 54,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody6", 0,
+    COMPARE_TO_NUMBER, 3, 55,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody7", 0,
+    COMPARE_TO_NUMBER, 3, 56,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody8", 0,
+    COMPARE_TO_NUMBER, 3, 57,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody9", 0,
+    COMPARE_TO_NUMBER, 3, 97,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody10", 0,
+    COMPARE_TO_NUMBER, 3, 98,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody11", 0,
+    COMPARE_TO_NUMBER, 3, 99,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody12", 0,
+    COMPARE_TO_NUMBER, 3, 100,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody13", 0,
+    COMPARE_TO_NUMBER, 3, 101,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody14", 0,
+    COMPARE_TO_NUMBER, 3, 102,
+    JUMP_IF_EQUAL, "parse8ByteValueLoopBody15", 0,
+
+    "parse8ByteValueLoopBody0",
+    MOVE_NUMBER, 0, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody1",
+    MOVE_NUMBER, 1, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody2",
+    MOVE_NUMBER, 2, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody3",
+    MOVE_NUMBER, 3, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody4",
+    MOVE_NUMBER, 4, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody5",
+    MOVE_NUMBER, 5, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody6",
+    MOVE_NUMBER, 6, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody7",
+    MOVE_NUMBER, 7, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody8",
+    MOVE_NUMBER, 8, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody9",
+    MOVE_NUMBER, 9, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody10",
+    MOVE_NUMBER, 10, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody11",
+    MOVE_NUMBER, 11, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody12",
+    MOVE_NUMBER, 12, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody13",
+    MOVE_NUMBER, 13, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody14",
+    MOVE_NUMBER, 14, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBody15",
+    MOVE_NUMBER, 15, 4,
+    JUMP, "parse8ByteValueLoopBodyContinue", 0,
+
+    "parse8ByteValueLoopBodyContinue",
+    SHIFT_LEFT_BY_NUMBER, 4, 2,
+    ADD, 4, 2,
+    ADD_NUMBER, 8, 0,
+    JUMP, "parse8ByteValueLoop", 0,
+
+    "parse8ByteValueLoopExit",
+    MOVE, 2, -4,
+    RETURN, 0, 0,
+
+    "print8ByteValue",
+    PUSH_NUMBER, 60, 0,
+    PUSH_NUMBER, 0, 0,
+    PUSH_NUMBER, 0, 0,
+    JUMP, "print8ByteValueLoop", 0,
+
+    "print8ByteValueLoop",
+    COMPARE_TO_NUMBER, 0, 0,
+    JUMP_IF_GREATER, "print8ByteValueLoopBody", 0,
+    COMPARE_TO_NUMBER, 0, 0,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody", 0,
+    JUMP, "print8ByteValueLoopExit", 0,
+
+    "print8ByteValueLoopBody",
+    MOVE, -3, 1,
+    SHIFT_RIGHT, 0, 1,
+    BITWISE_AND_WITH_NUMBER, 15, 1,
+    MOVE_NUMBER, 48, 2,
+    COMPARE_TO_NUMBER, 1, 0,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody0", 0,
+    COMPARE_TO_NUMBER, 1, 1,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody1", 0,
+    COMPARE_TO_NUMBER, 1, 2,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody2", 0,
+    COMPARE_TO_NUMBER, 1, 3,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody3", 0,
+    COMPARE_TO_NUMBER, 1, 4,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody4", 0,
+    COMPARE_TO_NUMBER, 1, 5,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody5", 0,
+    COMPARE_TO_NUMBER, 1, 6,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody6", 0,
+    COMPARE_TO_NUMBER, 1, 7,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody7", 0,
+    COMPARE_TO_NUMBER, 1, 8,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody8", 0,
+    COMPARE_TO_NUMBER, 1, 9,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody9", 0,
+    COMPARE_TO_NUMBER, 1, 10,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody10", 0,
+    COMPARE_TO_NUMBER, 1, 11,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody11", 0,
+    COMPARE_TO_NUMBER, 1, 12,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody12", 0,
+    COMPARE_TO_NUMBER, 1, 13,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody13", 0,
+    COMPARE_TO_NUMBER, 1, 14,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody14", 0,
+    COMPARE_TO_NUMBER, 1, 15,
+    JUMP_IF_EQUAL, "print8ByteValueLoopBody15", 0,
+
+    "print8ByteValueLoopBody0",
+    MOVE_NUMBER, 48, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody1",
+    MOVE_NUMBER, 49, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody2",
+    MOVE_NUMBER, 50, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody3",
+    MOVE_NUMBER, 51, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody4",
+    MOVE_NUMBER, 52, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody5",
+    MOVE_NUMBER, 53, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody6",
+    MOVE_NUMBER, 54, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody7",
+    MOVE_NUMBER, 55, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody8",
+    MOVE_NUMBER, 56, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody9",
+    MOVE_NUMBER, 57, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody10",
+    MOVE_NUMBER, 97, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody11",
+    MOVE_NUMBER, 98, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody12",
+    MOVE_NUMBER, 99, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody13",
+    MOVE_NUMBER, 100, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody14",
+    MOVE_NUMBER, 101, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBody15",
+    MOVE_NUMBER, 102, 2,
+    JUMP, "print8ByteValueLoopBodyContinue", 0,
+
+    "print8ByteValueLoopBodyContinue",
+    CALL, "writeToTranscript", 0,
+    ADD_NUMBER, -4, 0,
+    JUMP, "print8ByteValueLoop", 0,
+
+    "print8ByteValueLoopExit",
+    RETURN, 0, 0,
+]
+
+
 class TkScreen:
     def __init__(self):
         self.pending_key = None
@@ -47,7 +671,11 @@ class TkScreen:
         self.root.bind("<KeyPress>", self.on_key)
 
     def on_key(self, event):
-        if event.char:
+        if event.keysym == "Return":
+            self.pending_key = 10
+        elif event.keysym == "BackSpace":
+            self.pending_key = 8
+        elif event.char:
             self.pending_key = ord(event.char)
 
     def read_key(self):
@@ -64,6 +692,8 @@ class TkScreen:
             for x in range(128):
                 address = console_address + (y * 128 + x) * 8
                 value = asint(memory[address : address + 8])
+                if value == 0:
+                    value = ord(" ")
                 row.append(chr(value & 255))
             rows.append("".join(row))
 
@@ -80,6 +710,27 @@ def keyboard_step(memory, screen):
         memory[1000056 : 1000056 + 8] = as8(key)
         memory[1000048 : 1000048 + 8] = [0] * 8
     return memory
+
+
+def disk_step(memory, disk):
+    is_waiting_for_disk_read = asint(memory[1000032 : 1000032 + 8])
+    is_waiting_for_disk_write = asint(memory[1000040 : 1000040 + 8])
+
+    if is_waiting_for_disk_read == 1:
+        disk_address = asint(memory[1000008 : 1000008 + 8])
+        memory_address = asint(memory[1000016 : 1000016 + 8])
+        num_bytes = asint(memory[1000024 : 1000024 + 8])
+        memory[memory_address : memory_address + num_bytes] = disk[disk_address : disk_address + num_bytes]
+        memory[1000032 : 1000032 + 8] = as8(0)
+
+    if is_waiting_for_disk_write == 1:
+        disk_address = asint(memory[1000008 : 1000008 + 8])
+        memory_address = asint(memory[1000016 : 1000016 + 8])
+        num_bytes = asint(memory[1000024 : 1000024 + 8])
+        disk[disk_address : disk_address + num_bytes] = memory[memory_address : memory_address + num_bytes]
+        memory[1000040 : 1000040 + 8] = as8(0)
+
+    return memory, disk
 
 
 def cpu_step(memory, equal_flag, greater_flag):
@@ -254,6 +905,9 @@ if __name__ == "__main__":
     ]:
         disk[address : address + 8] = as8(value)
 
+    operating_system = assemble(operating_system_source, 24)
+    disk[24 : 24 + len(operating_system)] = operating_system
+
     memory[:] = disk[:]
     equal_flag = 0
     greater_flag = 0
@@ -261,6 +915,7 @@ if __name__ == "__main__":
 
     while True:
         memory = keyboard_step(memory, screen)
+        memory, disk = disk_step(memory, disk)
         memory, equal_flag, greater_flag = cpu_step(memory, equal_flag, greater_flag)
         memory = console_step(memory, screen)
-        time.sleep(0.05)
+        time.sleep(0.001)
--- tests.py
+++ tests.py
@@ -49,6 +49,70 @@ def run_steps(memory, equal_flag, greater_flag, count):
     return memory, equal_flag, greater_flag
 
 
+def label_addresses(source, start_address):
+    labels = {}
+    address = start_address
+    i = 0
+
+    while i < len(source):
+        if isinstance(source[i], str):
+            labels[source[i]] = address
+            i += 1
+        else:
+            address += 24
+            i += 3
+
+    return labels
+
+
+def boot_os():
+    disk = [0] * 4000000
+    memory = [0] * len(disk)
+
+    write8(disk, 0, 24)
+    write8(disk, 8, 500000)
+    write8(disk, 16, 500000)
+
+    operating_system = computer.assemble(computer.operating_system_source, 24)
+    disk[24 : 24 + len(operating_system)] = operating_system
+    memory[:] = disk[:]
+
+    return memory, disk, 0, 0
+
+
+def run_os_with_keys(keys, stop, max_cycles=1000000):
+    memory, disk, equal_flag, greater_flag = boot_os()
+    keys = list(keys)
+
+    for _ in range(max_cycles):
+        if read8(memory, 1000048) == 1 and keys:
+            write8(memory, 1000056, keys.pop(0))
+            write8(memory, 1000048, 0)
+
+        memory, disk = computer.disk_step(memory, disk)
+        memory, equal_flag, greater_flag = computer.cpu_step(memory, equal_flag, greater_flag)
+
+        if stop(memory, keys):
+            return memory, disk
+
+    raise AssertionError("OS test did not finish")
+
+
+def transcript(memory):
+    end = read8(memory, 1000000)
+    result = []
+    for address in range(2000000, end, 8):
+        result.append(chr(read8(memory, address) & 255))
+    return "".join(result)
+
+
+def console_prefix(memory, length):
+    result = []
+    for address in range(1000072, 1000072 + length * 8, 8):
+        result.append(chr(read8(memory, address) & 255))
+    return "".join(result)
+
+
 def test_idle():
     memory, equal_flag, greater_flag = reset(instruction(0))
     memory, _, _ = computer.cpu_step(memory, equal_flag, greater_flag)
@@ -156,6 +220,37 @@ def test_call_and_return():
     assert read8(memory, slot(memory, 1)) == 123
 
 
+def test_os_echoes_typed_character():
+    memory, _ = run_os_with_keys(
+        [ord("a")],
+        lambda memory, keys: not keys and console_prefix(memory, 1) == "a",
+    )
+    assert transcript(memory) == "a"
+    assert console_prefix(memory, 1) == "a"
+
+
+def test_os_invokes_read_disk_program():
+    labels = label_addresses(computer.operating_system_source, 24)
+    program_start = labels["readFromDiskProgram"]
+    program_length = labels["writeToDiskProgram"] - program_start
+    command = (
+        f"{program_start:016x}"
+        f"{program_length:016x}"
+        f"{0:016x}"
+        f"{8:016x}"
+    )
+    expected_output = "0000000000000018"
+    expected = command + "\n" + expected_output
+
+    memory, _ = run_os_with_keys(
+        [ord(character) for character in command] + [10],
+        lambda memory, keys: not keys and console_prefix(memory, len(expected)) == expected,
+    )
+
+    assert transcript(memory) == expected
+    assert console_prefix(memory, len(expected)) == expected
+
+
 def run(test):
     test()
     print(f"success: {test.__name__}")
@@ -172,3 +267,5 @@ if __name__ == "__main__":
     run(test_compare_and_jump_if_greater)
     run(test_jump)
     run(test_call_and_return)
+    run(test_os_echoes_typed_character)
+    run(test_os_invokes_read_disk_program)
